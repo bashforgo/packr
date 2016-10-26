2f90eae493c0faa68bdc884b84b354207639776c
SpeedoDevo <speedo@devo.hu>
2016-10-26 19:47:09 +0100

    added webpack progress

--- conf/webpack.conf.js
+++ conf/webpack.conf.js
@@ -27,6 +27,24 @@ module.exports = mergeWith({}, base, {
     ]
   },
   plugins: [
+    new webpack.ProgressPlugin(function defaultHandler(percentage, msg) {
+      var details = Array.prototype.slice.call(arguments, 2);
+      if(percentage < 1) {
+        percentage = Math.floor(percentage * 100);
+        msg = percentage + "% " + msg;
+        if(percentage < 100) {
+          msg = " " + msg;
+        }
+        if(percentage < 10) {
+          msg = " " + msg;
+        }
+        details.forEach(function(detail) {
+          if(!detail) return;
+          msg += " " + detail
+        });
+      }
+      process.stderr.write(msg + '\n');
+    }),
     new webpack.optimize.OccurrenceOrderPlugin(),
     new webpack.NoErrorsPlugin(),
     new HtmlWebpackPlugin({
