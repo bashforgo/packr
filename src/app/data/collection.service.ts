3da430a0b5dc8f78291d3ac2c99ab2e85d771a0d
SpeedoDevo <speedo@devo.hu>
2016-11-23 22:34:50 +0000

    memoized collection generation

--- src/app/data/collection.service.ts
+++ src/app/data/collection.service.ts
@@ -1,10 +1,13 @@
 import { Injectable } from '@angular/core';
-import { PacksGeneratorService } from './';
-import { DisplayCard, Packs, CardClassDictionary, CostDictionary, Rarity, Cost } from './types';
+import { PacksGeneratorService, PacksOpenerService } from './';
+import { DisplayCard, Packs, CardClassDictionary, CostDictionary, Rarity, Cost, Pack } from './types';
 import Dictionary = _.Dictionary;
 
 type Card = DisplayCard;
 type Collection = CardClassDictionary<CostDictionary<Dictionary<number>>>;
+type CollectionIOSignature = [Collection, {}, Packs];
+type CollectionResetSignature =
+  [Collection, {}, ((pks : Packs) => CollectionIOSignature)];
 
 @Injectable()
 export class CollectionService {
@@ -13,35 +16,46 @@ export class CollectionService {
   public rarityBreakdown;
   private _events;
 
-  constructor(pgs : PacksGeneratorService) {
-    //potentially memoize so that it is not recalculated for added packs
+  constructor(pgs : PacksGeneratorService, pos : PacksOpenerService) {
     this._events = pgs.events
-      .map((packs : Packs) => {
-          const collection : Collection = {};
-          const rarityBreakdown = {};
-          return [collection, rarityBreakdown, _(packs)
-            .map(pack => _(pack)
-              .map(card => {
-                const { cardClass, cost, detail, name, rarity } = card;
-                const path = [cardClass, cost, name];
-                const count = (_.get<number>(collection, path) || 0) + 1;
-                _.set(collection, path, count);
+      .withLatestFrom(
+        pos.events
+          .do(e => console.log(e))
+          .map(() => {
+            const collection = {};
+            const rarityBreakdown = {};
+            const _packsProcessor = _.memoize((pack : Pack) => {
+              return _.map(
+                pack,
+                (card : Card) => {
+                  const { cardClass, cost, detail, name, rarity } = card;
+                  const path = [cardClass, cost, name];
+                  const count = (_.get<number>(collection, path) || 0) + 1;
+                  _.set(collection, path, count);
 
-                path[0] = rarity;
-                _.set(rarityBreakdown, path, count);
-                path[1] = Cost.other(cost);
-                const otherCount = _.get<number>(rarityBreakdown, path) || 0;
-                path[1] = 'total';
-                _.set(rarityBreakdown, path, count + otherCount);
+                  path[0] = rarity;
+                  _.set(rarityBreakdown, path, count);
+                  path[1] = Cost.other(cost);
+                  const otherCount = _.get<number>(rarityBreakdown, path) || 0;
+                  path[1] = 'total';
+                  _.set(rarityBreakdown, path, count + otherCount);
 
-                return {
-                  extra: Rarity.isExtra(rarity, count + 1),
-                  cardClass, cost, detail, name, rarity
-                };
-              }).value()
-            ).value()];
-        }
-      ).share();
+                  return {
+                    extra: Rarity.isExtra(rarity, count),
+                    cardClass, cost, detail, name, rarity
+                  };
+                }
+              );
+            });
+
+            const packsProcessor = (pks) => [collection, rarityBreakdown, _.map(pks, _packsProcessor)];
+
+            return [collection, rarityBreakdown, packsProcessor];
+          })
+          .share()
+      )
+      .map(([pks, [, , pksProc]] : [Packs, CollectionResetSignature]) => pksProc(pks))
+      .share();
 
     this.events = this._events
       .map(([coll, ]) => coll);
