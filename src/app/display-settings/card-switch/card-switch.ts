896a953a4db08a8f2483f06ff66ff31bb20b2473
SpeedoDevo <speedo@devo.hu>
2017-03-22 21:03:35 +0000

    card themed switch

---
+++ src/app/display-settings/card-switch/card-switch.ts
@@ -0,0 +1,23 @@
+import { Component, ChangeDetectionStrategy, Input, Output } from '@angular/core';
+import { Subject } from 'rxjs';
+import { DisplayCard } from '../../data/types';
+
+@Component({
+  selector: 'pr-card-switch',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  template
+})
+export class CardSwitchComponent {
+  @Input() value : boolean;
+  @Input() onData : DisplayCard;
+  @Input() offData : DisplayCard;
+  @Output() onClicked = new Subject<boolean>();
+
+  toggle() {
+    this.onClicked.next(this.value);
+  }
+
+  get currentData() {
+    return this.value ? this.onData : this.offData;
+  }
+}
