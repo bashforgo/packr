16c9ae448aecb1c91e954bb91d1ff39537e67665
Barnabas Forgo <Barnabas.Forgo@mhr.co.uk>
2017-03-23 11:19:51 +0000

    reverse filter toggling after all

--- src/app/display-settings/card-filter/card-filter.service.ts
+++ src/app/display-settings/card-filter/card-filter.service.ts
@@ -20,7 +20,7 @@ class Filters {
   }
 
   get all() : boolean {
-    return _.isEqual(this, Filters.defaults)
+    return _.isEqual(this, Filters.defaults);
   }
 
   set all(value : boolean) {
@@ -36,24 +36,28 @@ class Filters {
 
 @Injectable()
 export class CardFilterService {
-  private _value = Filters.defaults;
-  readonly events = new BehaviorSubject<Filters>(this._value);
-
-  private getterSetter(prop : string[]) {
-    return (val : boolean) => {
-      if (!_.isUndefined(val)) {
-        _.set(this._value, prop, val);
-        this.events.next(this._value)
-      } else {
-        return _.get(this._value, prop)
-      }
-    }
-  }
-
   comn = this.getterSetter(['byRarity', 'comn']);
   rare = this.getterSetter(['byRarity', 'rare']);
   epic = this.getterSetter(['byRarity', 'epic']);
   lgnd = this.getterSetter(['byRarity', 'lgnd']);
   gold = this.getterSetter(['gold']);
   all = this.getterSetter(['all']);
+
+  readonly events;
+  private _value;
+  constructor() {
+    this._value = Filters.defaults;
+    this.events = new BehaviorSubject<Filters>(this._value);
+  }
+
+  private getterSetter(prop : string[]) {
+    return (val? : boolean) => {
+      if (!_.isUndefined(val)) {
+        _.set(this._value, prop, val);
+        this.events.next(this._value);
+      } else {
+        return _.get(this._value, prop);
+      }
+    };
+  }
 }
